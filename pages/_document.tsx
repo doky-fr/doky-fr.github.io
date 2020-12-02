fc91bf110c48cfae7726123c8cdd5b3823de1fc3
Fabien Caylus <fabien.caylus@gmail.com>
2020-12-02 19:16:47 +0100

    Update page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,6 +54,9 @@ export default class MyDocument extends Document {
                     <meta name="twitter:description" content={siteDescription} />
                     <meta name="twitter:image" content="/android-chrome-512x512.png" />
                     <meta name="twitter:image:alt" content={siteName} />
+
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
                 <Main />
