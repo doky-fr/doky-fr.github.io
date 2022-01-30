5ad042be2c31151a44414babb539f49b1f83d0f9
fcaylus <fcaylus@users.noreply.github.com>
2022-01-30 10:14:21 +0000

    deploy: 9bfb2ff9919dc7bc751cf1ea3befbd8f5c14a780

--- _next/static/myEobK56Rds931gee-qFa/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-dd46baf47005f57a257d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
